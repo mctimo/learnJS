4544d0c9ec7aaec287393b772e948c2c8b775c16
Artem.Ostapenko <timfenomen@gmail.com>
2024-07-27 22:28:36 +0200

    added gitignore

--- JavaScript+React/1-BasedJS/11-SimpleObject/script.js
+++ JavaScript+React/1-BasedJS/11-SimpleObject/script.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 const storeName = 'Apple';
 
+// eslint-disable-next-line no-unused-vars
 const storeDescription = {
     budget: 10000,
     employees: ['Oleg', 'Nikita', 'Denis'],
     products: { iphone: 1000, ipad: 1200 },
     open: true
-}
+};
--- JavaScript+React/1-BasedJS/12-interactionWithUser/script.js
+++ JavaScript+React/1-BasedJS/12-interactionWithUser/script.js
@@ -5,8 +5,9 @@
 // const answer = prompt("How old are you?", "18")
 // console.log(answer)
 
-const answers = []
-const questions = ['Ваше имя?', 'Ваша фамилия?', 'Сколько вам лет?']
+
+const answers = [];
+const questions = ['Ваше имя?', 'Ваша фамилия?', 'Сколько вам лет?'];
 
 for (let i = 0; i < questions.length; i++) {
     answers[i] = prompt(questions[i]);
